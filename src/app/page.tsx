ace375c4499244ef31c93f0736c0dfd89ce064d5
Ar4rN1c0 <113354801+Ar4rN1c0@users.noreply.github.com>
2023-10-17 10:09:11 +0000

    Styles upgrade

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,8 +7,16 @@ export default function HomePage () {
                 Choose a fishtank please
             </h1>
         <section className="main-section">
-            <Link href="fishtank-selection/1" className="fishtank-select">Fishtank Marie Curie</Link>
-            <Link href="fishtank-selection/2" className="fishtank-select">Fishtank Leornardo Da Vinci</Link>
+            <Link href="fishtank-selection/1" className="fishtank-select">
+                <p>
+                    Fishtank Marie Curie
+                </p>
+            </Link>
+            <Link href="fishtank-selection/2" className="fishtank-select">
+                <p>
+                Fishtank Leornardo Da Vinci
+                </p>
+            </Link>
 
         </section>
         </section>
